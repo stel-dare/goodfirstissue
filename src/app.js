0781e964440a7d64c7d12a60b1ebffa3f1f84c74
Stella Dare <stelladare2@gmail.com>
2021-05-31 22:27:52 +0000

    Initial setup with netlify functions

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const serverless = require('serverless-http');
+
+
+const app = express();
+
+const router = express.Router();
+
+
+router.get('/', (req,res)=>{
+    res.json({
+        "hello" : "hi"
+    })
+})
+
+app.use('/.netlify/functions/app', router);
+
+module.exports.handler = serverless(app);
